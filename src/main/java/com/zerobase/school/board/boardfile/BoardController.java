bd966c849aba3d82c40d975d99906d7c3e790672
mgcztech576 <127584002+mgcztech576@users.noreply.github.com>
2023-11-14 20:58:40 +0900

    aa

--- src/main/java/com/zerobase/school/board/boardfile/BoardController.java
+++ src/main/java/com/zerobase/school/board/boardfile/BoardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController @RequiredArgsConstructor
-public class BoardController {
+public class BoardController {//[2]
     private final BoardService boardService;
     @DeleteMapping("/board/delete/{id}")
     public void boardDelete(@PathVariable Long id){
